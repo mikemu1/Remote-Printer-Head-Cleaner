c92c1f3081bd78c1328f777f09b454d3f6f9fd1d
Mike Mueller <mikemu1@comcast.net>
2021-09-23 22:04:40 -0700

    Prettyclean, beach printer, working

---
+++ pretty.py
@@ -0,0 +1,3 @@
+MY_EMAIL_ADDRESS = 'mikemu0919@gmail.com'
+MY_EMAIL_PASSWORD = 'vbwb ppnp yqtl ujyg'
+
---
+++ prettyclean.py
@@ -0,0 +1,62 @@
+import smtplib
+from email.message import EmailMessage
+from datetime import datetime
+from pretty import *
+
+dts = datetime.now().ctime()
+print(dts)
+
+TO_EMAIL_ADDRESS = '88b34hjm@hpeprint.com'
+# TO_EMAIL_ADDRESS = 'mikemu0919@icloud.com'
+
+txt = f"Clean printer heads {dts}"
+
+EMAIL_CONTENT = f'''
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <h1>Cleaning printheads</b></h1>
+
+        <p style="font-size:14px">{txt}</br>
+        <span style="color:blue">{txt}</span></br>
+        <span style="color:red">{txt}</span></br>
+        <span style="color:yellow; background-color:gray">{txt}</span></p>
+
+        <p style="font-size:18px">{txt}</br>
+        <span style="color:blue">{txt}</span></br>
+        <span style="color:red">{txt}</span></br>
+        <span style="color:yellow; background-color:gray">{txt}</span></p>
+
+        <p style="font-size:18px;font-weight:bold">{txt}</br>
+        <span style="color:blue">{txt}</span></br>
+        <span style="color:red">{txt}</span></br>
+        <span style="color:yellow; background-color:gray">{txt}</span></p>
+
+        <p style="font-size:22px">{txt}</br>
+        <span style="color:blue">{txt}</span></br>
+        <span style="color:red">{txt}</span></br>
+        <span style="color:yellow; background-color:gray">{txt}</span></p>
+
+   </body>
+</html>
+'''
+
+msg = EmailMessage()
+msg['Subject'] = f'PrettyClean {dts}'
+msg['From'] = MY_EMAIL_ADDRESS 
+msg['To'] = TO_EMAIL_ADDRESS 
+msg.set_content(EMAIL_CONTENT, subtype='html')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    print(smtp.login(MY_EMAIL_ADDRESS, MY_EMAIL_PASSWORD))
+    print(smtp.send_message(msg))
+
+    
+
+    
+
+
+
+
